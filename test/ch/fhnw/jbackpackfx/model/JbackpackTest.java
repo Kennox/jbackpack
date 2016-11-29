0f66a50cbe51c7e2f74a2db7052e3d9670bf4b32
Gyssim <simon.gysin@students.fhnw.ch>
2016-11-29 10:08:49 +0100

    created test class

---
+++ test/ch/fhnw/jbackpackfx/model/JbackpackTest.java
@@ -0,0 +1,22 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ */
+public class JbackpackTest {
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @Test
+  public void sampleTest() {
+
+  }
+
+}
